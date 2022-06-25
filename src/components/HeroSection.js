e1923bc5620b4826e7c9e427d361827964fb2258
nsuarezl <91982403+nsuarezl@users.noreply.github.com>
2022-06-24 20:39:38 -0400

    commit on main

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,23 +7,33 @@ import Typewriter from "typewriter-effect";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <h1>
+      <div className="hero-firsthing">
+        COPYRIGHT 2002-2022 © NSL INDUSTRIES
+      </div>
+      <div className="hero-header">
         <Typewriter
           onInit={(typewriter) => {
             typewriter
-              .typeString("Hello, Nelson here\n")
-              .pauseFor(250)
-              .typeString("sometimes I code things.")
+              .typeString(">Hello, Nelson here.")
               .start();
           }}
         />
-      </h1>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .pauseFor(3500)
+              .typeString(">I code things sometimes.")
+              .start();
+          }}
+        />
+      </div>
+    
       <div className="hero-intro">
         I'm a computer science student based in Central Florida. I have an
-        interest in front-end development, artificial intelligence and all
+        interest in front-end development, artificial intelligence, music and all
         things in between.
       </div>
-
+ 
     </div>
   );
 }
