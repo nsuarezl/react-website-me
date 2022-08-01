bd99562db1f6beb60b5755b0c8112b68c37c219d
nsuarezl <91982403+nsuarezl@users.noreply.github.com>
2022-08-01 16:15:15 -0400

    Add files via upload

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,30 +1,31 @@
 import React from "react";
 import "./Cards.css";
 import CardItem from "./CardItem";
+import FadeIn from "./pages/FadeIn";
 
 function Cards() {
   return (
-    <div className="cards">
-      <h1> Check out these EPIC Destinations!</h1>
-      <div className="cards__container">
-        <div className="cards__wrapper">
-          <ul className="cards__items">
-            <CardItem
-              src="images/folderItem.jpg"
-              text="Explore the hidden"
-              label="Yo mama"
-              path="/projects"
-            />
-            <CardItem
-              src="images/folderItem.jpg"
-              text="Explore the hidden"
-              label="Yo mama"
-              path="/projects"
-            />
-          </ul>
+      <div className="cards">
+        <h1> About Me</h1>
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              <CardItem
+                src="images/folderItem.jpg"
+                text="Explore the hidden"
+                label="Yo mama"
+                path="https://google.com"
+              />
+              <CardItem
+                src="images/folderItem.jpg"
+                text="Explore the hidden"
+                label="Yo mama"
+                path="/projects"
+              />
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
