6cd9404642babed1f73b1ba3ada86521524873f3
nsuarezl <91982403+nsuarezl@users.noreply.github.com>
2022-08-01 16:15:45 -0400

    Add files via upload

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,6 +1,46 @@
 import React from "react";
+import Typist from "react-typist";
 import "../../App.css";
+import Cards from "../Cards";
+import FadeInSection from "./FadeIn";
+import "../Styles/AboutMe.css";
+import Fade from 'react-reveal/Fade';
 
-export default function AboutMe() {
-  return <h1 className="about-me">ABOUT ME</h1>;
+
+
+function AboutMe() {
+  return (
+    <Fade up delay={6000} duration={1000}>
+      <div className="about-container"> 
+          <Typist className="about-header">
+            <Typist.Delay ms={7000}/>
+          /about me
+          </Typist>
+
+          <div className="about-intro">
+            I am a senior at <a href="https://floridapoly.edu" target="_blank"> Florida Poly </a>
+            {" "}currently studying <b>computer science</b>
+          </div>
+          <div className="about-intro">
+            Over the past few years I have used:
+            
+            <ul>
+              <br></br>
+              <li><b>Java</b></li>
+              <li><b>React.js</b></li>
+              <br></br>
+              <li><b>C++</b></li>
+              <li><b>HTML and CSS</b></li>
+            </ul>
+            <div className="about-intro-p">
+            Whenever I am not coding stuff, I like to play videogames and consume TV shows.
+            </div>
+          </div>
+
+      </div>
+    </Fade>
+    
+  );
 }
+
+export default AboutMe;
---
+++ src/components/pages/Experience.js
@@ -0,0 +1,44 @@
+
+import React, { useEffect, useState } from "react";
+import "../../App.css";
+import Fade from 'react-reveal/Fade';
+import Typist from "react-typist";
+import "../Styles/Experience.css";
+
+class Experience extends React.Component {
+
+  state = {
+    text: 'something'
+  }
+
+  onClickButton1 = () => {
+    this.setState({
+      text: 'clicked 1'
+    });
+  }
+
+  onClickButton2 = () => {
+    this.setState({
+      text: 'clicked 2'
+    });
+  }
+  render(){
+  return (
+    <Fade up delay={7000} duration={1000}>
+      <div className="exp-container"> 
+          <Typist className="exp-header">
+          <Typist.Delay ms={8000}/>
+          /experience
+          </Typist>
+        <button onClick={() => this.onClickButton1()}>
+          Button 1
+        </button>
+        <button onClick={() => this.onClickButton2()}>
+          Button 2
+        </button>
+        <h1>{this.state.text}</h1>    
+      </div>
+    </Fade>
+  );}
+}
+export default Experience;
